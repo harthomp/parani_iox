4ee6764456b320d3246ced6473483045896539a0
harthomp <trickedyafool@gmail.com>
2024-03-27 10:58:26 +0800

    Added Config class with some parameters used for logging.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,19 @@
 import log
 import scan
 
+class Config:
+    logging_type = "file" # file || syslog
+    syslog_ipaddr = "192.168.1.3"
+
 if __name__ == "__main__":
-    logger = log.file_system_logger()
+    
+    config = Config()
+
+    if config.logging_type == "file":
+        logger = log.file_system_logger()
+    elif config.logging_type == "syslog":
+        print("TODO SYSLOG")
+    else:
+        exit()
+
     scan.app_main(logger)
